13bac805343ada87b55f200f744887209eadf0e6
Jackson5551 <45270054+Jackson5551@users.noreply.github.com>
2023-04-04 10:49:11 -0600

    finished through unit-06

--- src/app/components/departments/departments.component.ts
+++ src/app/components/departments/departments.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DepartmentsService } from 'src/app/services/departments.service';
+import { Department } from 'src/app/interfaces/department';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-departments',
   templateUrl: './departments.component.html',
   styleUrls: ['./departments.component.scss']
 })
-export class DepartmentsComponent {
+export class DepartmentsComponent implements OnInit {
+  departments: Department[] | undefined;
+  constructor(
+    private departmentsService: DepartmentsService,
+    private router: Router
+  ){}
 
+  goToDepartment(departmentId: string): void {
+    this.router.navigate(['./timesheet', { id: departmentId }]);
+  }
+
+  ngOnInit(): void {
+    this.departments = this.departmentsService.departments;
+  }
 }
