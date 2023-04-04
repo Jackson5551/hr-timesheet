cd85e2da01d5844962a57718ffec2a0a782d4ecc
Jackson5551 <45270054+Jackson5551@users.noreply.github.com>
2023-04-04 11:16:17 -0600

    unit-08

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { TimesheetComponent } from './components/timesheet/timesheet.component';
 import { AnalyticsComponent } from './components/analytics/analytics.component';
 import { TopNavbarComponent } from './components/top-navbar/top-navbar.component';
 import { AnalyticsTableComponent } from './components/analytics-table/analytics-table.component';
+import { FormsModule } from '@angular/forms';
 
 import { MaterialModule } from './modules/material/material.module';
 
@@ -26,6 +27,7 @@ import { MaterialModule } from './modules/material/material.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
